4ca416a67cd9b49280e697979403604e04b5f100
Little Clover <littleclover909@gmail.com>
2022-11-21 15:14:43 +0800

    minor fixes to server.c
    - incomplete prototypes `main()` -> `main(void)`
    - use `char[]` instead of `char*` for IP since there is no need for a
      dynamic memory allocation for it
    - import `<strings.h>` for `bzero()`

--- src/server.c
+++ src/server.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 
-int main()
+int main(void)
 {
     // user-defined server's ip and port
     /* Make sure both the client and server are having the same ip and port */
-    char *ip = "127.0.0.1";
+    char ip[] = "127.0.0.1";
     int port = 3939;
 
     int server_socket, client_socket;
