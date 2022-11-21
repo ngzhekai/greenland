f71488bce209591a7b7f254ee607cecfb29aa59c
Little Clover <littleclover909@gmail.com>
2022-11-21 15:16:52 +0800

    formatting

--- src/client.c
+++ src/client.c
@@ -7,56 +7,57 @@
 
 int main(void)
 {
-    // user-defined server's ip and port
-    /* Make sure both the client and server are having the same ip and port */
-    char ip[] = "127.0.0.1";
-    int port = 3939;
+  // user-defined server's ip and port
+  /* Make sure both the client and server are having the same ip and port */
+  char ip[] = "127.0.0.1";
+  int port = 3939;
 
-    int sock;
-    struct sockaddr_in addr;
-    // socklen_t addr_size;
-    char buffer[1024];
+  int sock;
+  struct sockaddr_in addr;
+  // socklen_t addr_size;
+  char buffer[1024];
 
-    // create the client side socket
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0)
-    {
-        perror("[-] Socket error!");
-        exit(1);
-    }
+  // create the client side socket
+  sock = socket(AF_INET, SOCK_STREAM, 0);
 
-    printf("[+] TCP server socket created. \n");
+  if (sock < 0) {
+    perror("[-] Socket error!");
+    exit(1);
+  }
 
-    // define the server address
-    memset(&addr, '\0', sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-    addr.sin_addr.s_addr = inet_addr(ip);
+  printf("[+] TCP server socket created. \n");
 
-    if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
-      perror("[-] Connection error!");
-      exit(2);
-    }
-    printf("Connected to the server. \n");
+  // define the server address
+  memset(&addr, '\0', sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(port);
+  addr.sin_addr.s_addr = inet_addr(ip);
 
-    // send the message to the server socket
-    char *msg = malloc(1024);
-    printf("Enter a message: ");
-    fgets(msg, 1024, stdin);
-    bzero(buffer, 1024);
-    strcpy(buffer, msg);
-    free(msg); // de-allocate the memory
-    printf("Client: %s\n", buffer);
-    send(sock, buffer, strlen(buffer), 0);
+  if (connect(sock, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+    perror("[-] Connection error!");
+    exit(2);
+  }
 
-    // receive the message from the server socket
-    bzero(buffer, 1024);
-    recv(sock, buffer, sizeof(buffer), 0);
-    printf("Server: %s\n", buffer);
+  printf("Connected to the server. \n");
 
-    // close the connection (socket)
-    close(sock);
-    printf("Disconnected from the server.\n");
+  // send the message to the server socket
+  char* msg = malloc(1024);
+  printf("Enter a message: ");
+  fgets(msg, 1024, stdin);
+  bzero(buffer, 1024);
+  strcpy(buffer, msg);
+  free(msg); // de-allocate the memory
+  printf("Client: %s\n", buffer);
+  send(sock, buffer, strlen(buffer), 0);
 
-    return 0;
+  // receive the message from the server socket
+  bzero(buffer, 1024);
+  recv(sock, buffer, sizeof(buffer), 0);
+  printf("Server: %s\n", buffer);
+
+  // close the connection (socket)
+  close(sock);
+  printf("Disconnected from the server.\n");
+
+  return 0;
 }
