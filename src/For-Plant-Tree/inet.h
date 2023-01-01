e455328032a1b964e444f7aac1216e7d6d8b8bf5
Ng Zhe Kai <ngzhekai@gmail.com>
2023-01-02 00:36:03 +0800

    added a client/server model for plant tree only (barely working)

---
+++ src/For-Plant-Tree/inet.h
@@ -0,0 +1,10 @@
+/* inet.h */
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#define SERV_TCP_PORT 25000
+#define SERV_UDP_PORT 35001
+#define CLI_UDP_PORT 35002
